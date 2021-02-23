ce65e97fce6b008bbe48b7d8d80adf560d92455d
H. Kamran <hkamran@unisontech.org>
2021-02-23 13:31:10 -0800

    Update download URL

--- Casks/quickocr.rb
+++ Casks/quickocr.rb
@@ -2,7 +2,7 @@ cask "quickocr" do
   version "1.0.0"
   sha256 "8348b65c5fe82d2a91bd6e5de013380fd91b13e7d6a3f775d8bb425a4994ee27"
 
-  url "https://github.com/aheze/QuickOCR/releases/download/#{version}/QuickOCR.zip"
+  url "https://github.com/aheze/QuickOCR/releases/download/v#{version}/QuickOCR.zip"
   name "QuickOCR"
   desc "Quickly run OCR on an image"
   homepage "https://github.com/aheze/QuickOCR"
